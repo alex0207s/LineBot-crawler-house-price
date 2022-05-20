b7c421f150e6f9f230e4967c8a1ebc9deece282d
alex0207 <alex640702070829@gmail.com>
2022-05-20 21:17:33 +0800

    app

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app_echo:app --preload
+web: gunicorn RequestHousePrice:app --preload
---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from linebot import LineBotApi, WebhookHandler
+
+import configparser
+
+app = Flask(__name__)
+
+# 讀取 config
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# LINE 聊天機器人的基本資料
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+
+from app import routes, model_for_line
