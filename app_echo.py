f2878247cf3dab1dff654501ed905af5773c8cb8
alex0207 <alex640702070829@gmail.com>
2022-05-20 14:53:43 +0800

    init

--- app_echo.py
+++ app_echo.py
@@ -7,8 +7,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 app = Flask(__name__)
 
 # LINE 聊天機器人的基本資料
-line_bot_api = LineBotApi('聊天機器人的 Chennel access token')
-handler = WebhookHandler('聊天機器人的 Channel secret')
+line_bot_api = LineBotApi('zM8h0i3xqlOstHiw0pGe5tHcnEc2yvJ6HRg6SRTH3Lg/1SlRdn3TWaZHePzvnhnMKshqAfriS0Hzowg20CzWa8UN+Zdtb+oE/3To/mtAZ0bX9uHXuxV2YB62b9Q45Z57OE9RdLakkeDJrkLQeDjmbgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('f0305d1d5dad008c4f5681e28413e4c6')
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
