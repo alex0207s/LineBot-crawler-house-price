03e696977afaa8891641685eba1ba9071545a84b
alex0207 <alex640702070829@gmail.com>
2022-05-20 21:29:10 +0800

    config

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # 讀取 config
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('../config.ini')
 
 # LINE 聊天機器人的基本資料
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
