a81e55c39f6d9a20dbce1a5afc710e04a5bd9e9d
alex0207 <alex640702070829@gmail.com>
2022-05-28 04:20:43 +0800

    record json file

--- app/models_for_line.py
+++ app/models_for_line.py
@@ -5,8 +5,15 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 def request_house_price():
     context = get_house_pirce_data()
+    result = sorted(context.items(), key=lambda x:x[1], reverse=True)[:5]
+    for index, record in enumerate(result):
+        text += str(index+1) + '. \n交易日期: ' + record[1][0] + '\n地址: ' + record[0] + '\n總價: ' + str(int(record[1][2].replace(',', ''))/10000) + ' 萬\n總面積: ' + record[1][1] + '\n\n'   
+        if len(text) >= 100:
+            break
+
+    return text
     # return handle_house_price_data(context)
-    return context
+    # return context
 
 @handler.add(MessageEvent, message=TextMessage)
 def RequestHousePrice(event):
