f1b645e9c6f5cac37c0afde5c4c2a4704691cb87
alex0207 <alex640702070829@gmail.com>
2022-05-27 19:16:46 +0800

    handle house price raw data

---
+++ handle_house_price_data.py
@@ -0,0 +1,20 @@
+target_address = [
+    '北投段', '北投堡段', '北投埔段', '頂茄荖段', '光華段', '青宅段', '新埔段', '新興段', '新光段',
+    '新豐段', '保安段', '中興段', '上林段', '和平段', '將軍段', '頂園段']
+
+def handle_house_price_data(raw_data):
+    house = {"交易日期":[], "地址":[], "總價":[], "總面積":[]}
+
+    for i in range(len(raw_data)):
+        deal = raw_data[i]
+        if deal['a'][:3] in target_address or deal['a'][:4] in target_address:
+            start_index = deal['a'].find('#') + 1 
+            house["交易日期"].append(deal['e'])
+            house["總面積"].append(deal['s'])
+            house["地址"].append(deal['a'][start_index::])
+            house["總價"].append(deal['tp'])
+
+    text = ""
+    for i in range(5):
+        text += str(i)+". " + raw_data[i]['a'] + "\t" + raw_data[i]['tp'] + "\n"
+    return text
