e3f7826e61df8d5f0b0f8021202b715221adfd96
alex0207 <alex640702070829@gmail.com>
2022-05-27 22:53:41 +0800

    house price message

--- handle_house_price_data.py
+++ handle_house_price_data.py
@@ -2,9 +2,9 @@ target_address = [
     '北投段', '北投堡段', '北投埔段', '頂茄荖段', '光華段', '青宅段', '新埔段', '新興段', '新光段',
     '新豐段', '保安段', '中興段', '上林段', '和平段', '將軍段', '頂園段']
 
-def display_message(records):
+def display_message(records, num=6):
     text = ''
-    for record in records.items():
+    for record in records.items()[:num]:
         text += '交易日期: ' + record[1][0] + '\n地址: ' + record[0] + '\n總價: ' + str(int(record[1][2].replace(',', ''))/10000) + ' 萬\n總面積: ' + record[1][1] + '\n\n'   
 
     return text
@@ -27,7 +27,7 @@ def handle_house_price_data(raw_data):
     
     print(records[::6])
     sorted(records.items(), key=lambda x:x[1], reverse=True)
-    return display_message(records[::6])
+    return display_message(records)
 
     # text = ""
     # for i in range(1,6):
