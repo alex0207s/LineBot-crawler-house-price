7d22e85cc48bab7c66627b69486995158c955b73
alex0207 <alex640702070829@gmail.com>
2022-05-28 15:14:00 +0800

    save house price data to json file

--- app/models_for_line.py
+++ app/models_for_line.py
@@ -4,14 +4,15 @@ from handle_house_price_data import handle_house_price_data
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 def request_house_price():
-    context = get_house_pirce_data()
-    result = sorted(context.items(), key=lambda x:x[1], reverse=True)[:5]
+    raw_data = get_house_pirce_data()
+    text = handle_house_price_data(raw_data)
+    # result = sorted(context, key=lambda x:x[1], reverse=True)[:5]
     
-    text = ''
-    for index, record in enumerate(result):
-        text += str(index+1) + '. \n交易日期: ' + record[1][0] + '\n地址: ' + record[0] + '\n總價: ' + str(int(record[1][2].replace(',', ''))/10000) + ' 萬\n總面積: ' + record[1][1] + '\n\n'   
-        if len(text) >= 300:
-            break
+    # text = ''
+    # for index, record in enumerate(result):
+    #     text += str(index+1) + '. \n交易日期: ' + record[1][0] + '\n地址: ' + record[0] + '\n總價: ' + str(int(record[1][2].replace(',', ''))/10000) + ' 萬\n總面積: ' + record[1][1] + '\n\n'   
+    #     if len(text) >= 300:
+    #         break
 
     return text
     # return handle_house_price_data(context)
